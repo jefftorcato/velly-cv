811d08b823a881af1cf108596b0e37ae5fc5c753
Jeff_Torcato <jefftorcato@gmail.com>
2018-05-14 19:15:56 +0530

    Added ajax call for form

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,74 @@ jQuery(function ($) {
 
     $('.left-col-block, .right-col-block').theiaStickySidebar();
 
+
+    function myFunction(msg) {
+
+        ("#snackbar").text(msg);
+        $("#snackbar").show();
+        
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ 
+            $("snackbar").hide(); 
+        }, 3000);
+    }
+
+    $("#contactForm").submit(function (event){
+
+        // Prevent default posting of form - put here to work in case of errors
+        event.preventDefault();
+
+        // Abort any pending request
+        if (request) {
+            request.abort();
+        }
+        // setup some local variables
+        var $form = $(this);
+
+        // Let's select and cache all the fields
+        var $inputs = $form.find("input, select, button, textarea");
+
+        // Serialize the data in the form
+        var serializedData = $form.serialize();
+
+        // Let's disable the inputs for the duration of the Ajax request.
+        // Note: we disable elements AFTER the form data has been serialized.
+        // Disabled form elements will not be serialized.
+        $inputs.prop("disabled", true);
+
+        // Fire off the request to /form.php
+        request = $.ajax({
+            url: "/php/sendemail.php",
+            type: "post",
+            data: serializedData
+        });
+    
+            // Callback handler that will be called on success
+            request.done(function (response, textStatus, jqXHR) {
+            // Log a message to the console
+            console.log("Hooray, it worked!");
+            var msg = "Thank you for contacting."
+            myFunction(msg);
+            $('#contactForm')[0].reset();
+        });
+
+        // Callback handler that will be called on failure
+        request.fail(function (jqXHR, textStatus, errorThrown) {
+            // Log the error to the console
+            console.error(
+                "The following error occurred: " +
+                textStatus, errorThrown
+            );
+            var msg = "Sorry.. couldn't send an email."
+            myFunction(msg);
+        });
+
+        // Callback handler that will be called regardless
+        // if the request failed or succeeded
+        request.always(function () {
+            // Reenable the inputs
+            $inputs.prop("disabled", false);   
+        });
+    });
+    
 }); // JQuery end
